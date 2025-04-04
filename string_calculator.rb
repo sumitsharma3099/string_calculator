456ee123e1cdfae602b2a30e7cdc4bd04d4534c0
Sumit sharma <39346001+sumitsharma3099@users.noreply.github.com>
2025-04-04 14:35:04 +0530

    Handle Negative Numbers and Raise Exception

--- string_calculator.rb
+++ string_calculator.rb
@@ -7,10 +7,18 @@ class StringCalculator
     if numbers.start_with?("//")
       delimiter_info, numbers = numbers.split("\n", 2)
       delimiters = delimiter_info[2..-1] # Remove the "//" prefix
-      numbers = numbers.split(delimiters).map(&:to_i).sum
+      numbers = numbers.split(delimiters).map(&:to_i)
     else
       # Default to handling commas and newlines
-      numbers = numbers.split(/[\n,]/).map(&:to_i).sum
+      numbers = numbers.split(/[\n,]/).map(&:to_i)
     end
+
+    # Handle negative numbers
+    negatives = numbers.select { |num| num < 0 }
+    if negatives.any?
+      raise "negatives not allowed: #{negatives.join(', ')}"
+    end
+
+    numbers.sum
   end
 end
