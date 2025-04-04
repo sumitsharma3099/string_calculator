57075b04b7c8fdbd32b40f672461d05b5622e3ba
Sumit sharma <39346001+sumitsharma3099@users.noreply.github.com>
2025-04-04 14:20:05 +0530

    Support Custom Delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,14 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    # Handle new lines and commas between numbers
-    numbers.split(/[\n,]/).map(&:to_i).sum
+    # Check for custom delimiters
+    if numbers.start_with?("//")
+      delimiter_info, numbers = numbers.split("\n", 2)
+      delimiters = delimiter_info[2..-1] # Remove the "//" prefix
+      numbers = numbers.split(delimiters).map(&:to_i).sum
+    else
+      # Default to handling commas and newlines
+      numbers = numbers.split(/[\n,]/).map(&:to_i).sum
+    end
   end
 end
