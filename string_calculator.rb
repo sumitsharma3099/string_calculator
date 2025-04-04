475117efa6eb0276494769fda03929e13ed030a5
Sumit sharma <39346001+sumitsharma3099@users.noreply.github.com>
2025-04-04 14:17:16 +0530

    Handle Single Number Input

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,8 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+
+    # Return the number if only one is provided
+    return numbers.to_i if numbers.include?(',')
   end
 end
