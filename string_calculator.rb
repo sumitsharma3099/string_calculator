b3e5b1162f828059244e39b00b1c1c5f6ede92dd
Sumit sharma <39346001+sumitsharma3099@users.noreply.github.com>
2025-04-04 14:18:18 +0530

    Handle Two Numbers Separated by Comma

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,7 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    # Return the number if only one is provided
-    return numbers.to_i if numbers.include?(',')
+    # Return the sum of two numbers
+    numbers.split(',').map(&:to_i).sum
   end
 end
